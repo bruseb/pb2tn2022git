03c7ee9dab504d1d8ee2a273dd13eee62e3c8940
Bruseb <brunosmerlo@gmail.com>
2022-08-31 15:48:27 -0300

    agrego patente y modelo

--- src/ar/unlam/gittn/Auto.java
+++ src/ar/unlam/gittn/Auto.java
@@ -1,5 +1,16 @@
 package ar.unlam.gittn;
 
 public class Auto {
+	
+	private String modelo;
+	private Integer patente;
+	
+	public Auto(String modelo, Integer patente) {
+		
+		this.modelo = modelo;
+		this.patente = patente;
+	}
+	
+	
 
 }
